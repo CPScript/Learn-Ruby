a943b1f0dbd6a4e1ca355567616d7ee920d36f5f
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-06 11:06:18 -0500

    Create 6 Formatting with String Methods.rb

---
+++ Forming the Form in Formatter/6 Formatting with String Methods.rb
@@ -0,0 +1,19 @@
+# Great! Now we’ve got our output, but as you can see, we haven’t used string methods to properly capitalize everything yet.
+
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from? "
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp
+state.upcase!
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
