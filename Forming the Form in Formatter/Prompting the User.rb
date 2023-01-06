892a915794fd56de6cbce8702ca19141cbb937bf
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-06 10:46:57 -0500

    Update Prompting the User.rb

--- Forming the Form in Formatter/Prompting the User.rb
+++ Forming the Form in Formatter/Prompting the User.rb
@@ -1,3 +1,5 @@
 # First, let’s write the code we’re already familiar with. In order to get input from the user, we’ll first need to print a prompt on the screen.
 
 
+# it should look like this
+print "Your question"
